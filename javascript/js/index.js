55d05b9cb3ca73097bc1a6aea26a2c375d93cd4b
Elizabeth <elizabethduong96@gmail.com>
2015-10-13 17:14:05 -0700

    Updated code in class

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,3 +1,28 @@
 /**
  * application script for index.html
  */
+
+document.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+
+    function forEachElement(collection, fn) {
+        for (var i=0; i<collection.length; i++) {
+            fn(collection[i]);
+        }
+    }
+
+    var clickMeButton = document.getElementById("click-me");
+    clickMeButton.addEventListener("click", function() {
+        var alerts = document.querySelectorAll('.alert');
+        ForEachelement(alert, function(alert) {
+            alert.style.display = "block";
+        });
+    });
+
+    var closeButtons = document.querySelectorAll('.alert .close');
+    forEachElement(closeButtons, function(button) {
+            button.addEventListener("click", function() {
+            button.parentElement.style.display = "none";
+        });
+    });
+});
