fa85c790d02730a512c13e0f8d46c8e947c10f53
Elizabeth Duong <johndoe@example.com>
2015-10-15 17:13:33 -0700

    jquery and leaflet

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -6,3 +6,19 @@
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
 
+// When DOM content has been loaded
+$(function() {
+	'use strict';
+	$('a').attr('target', '_blank');
+	$('article').hide().fadeIn(1000);
+
+	$('#toggle-article').click(function() {
+		$('article').fadeToggle();
+	});
+
+	$.getJSON('http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f')
+		.then(function(data) {
+			var temp = data.main.temp;
+			$('#temp').text(temp);
+		});
+});
